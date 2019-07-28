4f4c9150197638ea432a532b4d24503558e0b011
eugene.g@waardex.com <12345678>
2019-07-29 00:32:21 +0300

    update

--- 01/01-exercise/src/App.js
+++ 01/01-exercise/src/App.js
@@ -4,34 +4,57 @@ import subscribeToMessages from "./messages";
 import FadeIn from "./FadeIn";
 
 class PinScrollToBottom extends Component {
+
   render() {
+    const {scrollHeight, clientHeight, scrollTop} = document.documentElement;
+    const {pinToTop} = this.props;
+
+    console.log('scrollHeight' ,scrollHeight);
+    console.log('clientHeight' ,clientHeight);
+    console.log('scrollTop' ,scrollTop);
+
+    if (pinToTop) {
+      window.scrollTo(0, document.documentElement.scrollHeight)
+    }
     return this.props.children;
   }
 }
 
 class App extends Component {
   state = {
-    messages: []
+    messages: [],
+    scrolled: false,
+    pinToTop: true
   };
 
   componentDidMount() {
     subscribeToMessages(message => {
       this.setState({
-        messages: this.state.messages.concat([message])
+        messages: [...this.state.messages, message],
+        pinToTop: true
       });
     });
   }
 
+  // componentDidUpdate() {
+  //   this.setState()
+  // }
+
+  handleScroll = () => {
+    console.log(document.documentElement.scrollTop);
+    this.setState({scrolled: true});
+  };
+
   render() {
-    const { messages } = this.state;
+    const { messages, pinToTop } = this.state;
     return (
-      <div className="app">
+      <div className="app" onWheel={this.handleScroll}>
         <div className="link">
           <a href="https://www.youtube.com/watch?v=VKHFZBUTA4k&list=RDVKHFZBUTA4k">
             Sketch on YouTube
           </a>
         </div>
-        <PinScrollToBottom>
+        <PinScrollToBottom pinToTop={pinToTop}>
           <ol className="messages">
             {messages.map((message, index) => (
               <FadeIn key={index}>
